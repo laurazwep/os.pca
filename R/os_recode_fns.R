4003c61bb87c0204df4a0687a6fdee79706291d4
Laura Zwep <l.b.zwep@lacdr.leidenuniv.nl>
2025-08-07 14:16:04 +0200

    Use of styler to adapt spacing in scripts (https://www.tidyverse.org/blog/2017/12/styler-1.0.0/)

--- R/os_recode_fns.R
+++ R/os_recode_fns.R
@@ -1,12 +1,12 @@
 #' @noRd
-del_missing<-function(dat) {
-d<-dat[1,]*0
-for (i in 1:nrow(dat)) {
-    if (min(dat[i,])!=0) d<-rbind(d,dat[i,])
-    }
-    d<-(d[2:nrow(d),])
-    return(d)
- }
+del_missing <- function(dat) {
+  d <- dat[1, ] * 0
+  for (i in 1:nrow(dat)) {
+    if (min(dat[i, ]) != 0) d <- rbind(d, dat[i, ])
+  }
+  d <- (d[2:nrow(d), ])
+  return(d)
+}
 
 #' @noRd
 recode <- function(data) {

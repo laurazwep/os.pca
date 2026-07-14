649cb129a81ab2a63c7f3f53b01f13bc13c83ee9
Laura Zwep <l.b.zwep@lacdr.leidenuniv.nl>
2026-07-14 10:06:13 +0200

    Apadapted os_to_ordered function to apply to newly seen data. added option to use translation list

--- R/os_to_ordered.R
+++ R/os_to_ordered.R
@@ -5,35 +5,58 @@
 #'
 #' @param os_object Object with os_scores, os_loadings, os_data, os_catquants
 #' from the `os_pca` function
-#' @param data A data.frame input data for os_pca, if ommitted data from os_pca
+#' @param data A data.frame input data for os_pca, if omitted data from os_pca
 #' function can be used (only when `keep_data` is TRUE)
+#' @param trans_list A named list of ordered character vectors (as returned in
+#' `attr(cat_data, "os_levels")`), one per categorical column. If provided,
+#' `os_object$data` can be NULL.
 #'
-#'
-#' @returns Object with oredered categorical data (cat_data) and the levels of
+#' @returns Object with ordered categorical data (cat_data) and the levels of
 #' those ordered factors
 #'
 #' @export os_to_ordered
-os_to_ordered <- function(os_object, data) {
+os_to_ordered <- function(os_object, data, trans_list = NULL) {
   if (missing(data)) {
     if (is.null(os_object$data)) {
-      stop("Add data to data object!")
+      stop("Add original data to os_object or provide data to function")
     }
     data <- os_object$data
+    message("Changing original data to ordered factor")
   }
+
+  os_cols <- rownames(os_object$os_loadings)[os_object$level != "numerical"]
+
+
+  if (is.null(trans_list)) {
+    if (is.null(os_object$data)) {
+      stop("Add original data to os_object or add provide trans_list to function")
+    }
+
+    trans_list <- list()
+    for (column in os_cols) {
+      orig_data <- os_object$data[, column]
+      os_data <- os_object$os_data[, column]
+      lookup <- unique(data.frame(orig = orig_data, os = os_data))
+      trans_list[[column]] <- as.character(lookup$orig[order(lookup$os)])
+    }
+  }
+
+  # translate input data with os_levels
   cat_data <- data
-  os_cols <- names(cat_data)[os_object$level != "numerical"]
-  cat_data[, os_cols] <- os_object$os_data[, os_cols]
-  trans_list <- list()
+
   for (column in os_cols) {
-    t_df <- unique(data.frame(os = cat_data[, column], orig = data[, column]))
-    levels_os <- as.character(t_df[order(t_df$os), "orig"])
-    trans_list <- c(trans_list, list(levels_os))
-    cat_data[, column] <- factor(cat_data[, column], ordered = TRUE)
-    levels(cat_data[, column]) <- levels_os
+    levels_os <- trans_list[[column]]
+
+    if (anyNA(match(data[, column], levels_os))) {
+      unseen <- unique(data[is.na(match(data[, column], levels_os)), column])
+      warning(sprintf("Column '%s': %d value(s) not found in levels and set to NA: %s",
+        column, length(unseen), paste(unseen, collapse = ", ")))
+    }
+
+    cat_data[, column] <- factor(data[, column], levels = levels_os, ordered = TRUE)
   }
-  names(trans_list) <- os_cols
+
 
   attr(cat_data, "os_levels") <- trans_list
-
   return(cat_data)
 }

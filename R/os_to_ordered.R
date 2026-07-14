16a9b63d9c2d55908fa17c637d58b1dbcf4f7976
Laura Zwep <l.b.zwep@lacdr.leidenuniv.nl>
2026-07-14 20:58:49 +0200

    os_to_ordered fixed bug that loses levels in the transformation.

--- R/os_to_ordered.R
+++ R/os_to_ordered.R
@@ -15,7 +15,8 @@
 #' those ordered factors
 #'
 #' @export os_to_ordered
-os_to_ordered <- function(os_object, data, trans_list = NULL) {
+os_to_ordered <- function (os_object, data, trans_list = NULL)
+{
   if (missing(data)) {
     if (is.null(os_object$data)) {
       stop("Add original data to os_object or provide data to function")
@@ -23,40 +24,37 @@ os_to_ordered <- function(os_object, data, trans_list = NULL) {
     data <- os_object$data
     message("Changing original data to ordered factor")
   }
-
-  os_cols <- rownames(os_object$os_loadings)[os_object$level != "numerical"]
-
-
+  os_cols <- rownames(os_object$os_loadings)[os_object$level !=
+                                               "numerical"]
   if (is.null(trans_list)) {
     if (is.null(os_object$data)) {
       stop("Add original data to os_object or add provide trans_list to function")
     }
-
     trans_list <- list()
     for (column in os_cols) {
       orig_data <- os_object$data[, column]
       os_data <- os_object$os_data[, column]
       lookup <- unique(data.frame(orig = orig_data, os = os_data))
+      if (os_object$level[which(rownames(os_object$os_loadings) == column)] == "ordinal") {
+        lookup <- merge(data.frame(orig = ordered(levels(orig_data), levels(orig_data))),
+                        lookup, all.x = TRUE)
+        lookup$os <- approxfun(lookup$orig, lookup$os, rule = 2)(lookup$orig)
+      }
       trans_list[[column]] <- as.character(lookup$orig[order(lookup$os)])
     }
   }
-
-  # translate input data with os_levels
   cat_data <- data
-
   for (column in os_cols) {
     levels_os <- trans_list[[column]]
-
     if (anyNA(match(data[, column], levels_os))) {
-      unseen <- unique(data[is.na(match(data[, column], levels_os)), column])
+      unseen <- unique(data[is.na(match(data[, column],
+                                        levels_os)), column])
       warning(sprintf("Column '%s': %d value(s) not found in levels and set to NA: %s",
-        column, length(unseen), paste(unseen, collapse = ", ")))
+                      column, length(unseen), paste(unseen, collapse = ", ")))
     }
-
-    cat_data[, column] <- factor(data[, column], levels = levels_os, ordered = TRUE)
+    cat_data[, column] <- factor(data[, column], levels = levels_os,
+                                 ordered = TRUE)
   }
-
-
   attr(cat_data, "os_levels") <- trans_list
   return(cat_data)
 }
